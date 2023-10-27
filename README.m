cd190b6bef6199f773d2a5fed5c4ceea9327354e
Mehenaz Tabassum <145419797+meheanztabassum@users.noreply.github.com>
2023-10-27 16:54:31 +0600

    Update README.m

--- README.m
+++ README.m
@@ -1,9 +1,1 @@
-- 👋 Hi, I’m @meheanztabassum
-- 👀 I’m interested in programming and reading books
-- 🌱 I’m currently learning Python, Java, C, Html
-- 💞️ I’m looking to collaborate on ...
-- 📫 How to reach me ...
-
-<!---
-.
---->
+Hello! I am Mehenaz Tabassum
